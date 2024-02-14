d080adc6b412fab183d871f0d4d2b053dceb0e41
tjddn <tjddn3927@naver.com>
2024-02-14 23:14:31 +0900

    상품 수정 로직 spring boot test로 전환

--- code/tdd/product-order-api-tdd/product-order-service/src/test/java/com/example/productorderservice/product/ProductPatchServiceTest.java
+++ code/tdd/product-order-api-tdd/product-order-service/src/test/java/com/example/productorderservice/product/ProductPatchServiceTest.java
@@ -5,33 +5,38 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class ProductPatchServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private ProductPort mock;
-    @BeforeEach
-    void setup(){
-         mock = Mockito.mock(ProductPort.class);
-        productService = new ProductService(mock);
-    }
+
+    @Autowired
+    private ProductPort productPort;
 
     @Test
     void 상품수정() {
-        final Long productId = 1L;
 
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+
+        final Long productId = 1L;
         String name = "상품수정";
         int price = 10000;
         DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-        Product product = new Product("상품", 100, DiscountPolicy.NONE);
-        Mockito.when(mock.getProduct(productId)).thenReturn(product);
 
         UpdateProductRequest request = new UpdateProductRequest(name, price, discountPolicy);
-
         productService.updateProduct(productId, request);
+
+        GetProductResponse response = productService.getProduct(productId);
+
+        assertThat(response.name()).isEqualTo("상품수정");
+
     }
 
     @Test
